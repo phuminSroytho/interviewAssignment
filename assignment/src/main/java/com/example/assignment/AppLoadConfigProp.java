99c363d734bd6d9de075275805d039f335ab5aa0
PHUMIN SROYTHO <phumin.sroytho.work@gmail.com>
2024-02-20 14:43:18 +0700

    Number 1

--- assignment/src/main/java/com/example/assignment/AppLoadConfigProp.java
+++ assignment/src/main/java/com/example/assignment/AppLoadConfigProp.java
@@ -1,9 +1,12 @@
 package com.example.assignment;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+@Slf4j
 public class AppLoadConfigProp {
     public static final String VALUE_FORMAT = "Key : %s, \nValue : %s";
 
@@ -12,12 +15,13 @@ public class AppLoadConfigProp {
             String absolutePath = Thread.currentThread().getContextClassLoader().getResource("").getPath() + path;
             Properties loader = new Properties();
             loader.load(new FileInputStream(absolutePath));
-            loader.forEach((key, value) -> {
-                System.out.println(String.format("Key : %s, \nValue : %s", key, value));
-            });
+            loader.forEach((key, value) -> log.info(String.format(VALUE_FORMAT, key, value)));
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+    }
 
+    private AppLoadConfigProp() throws IllegalAccessException {
+        throw new IllegalAccessException();
     }
 }
--- assignment/src/main/java/com/example/assignment/AssignmentApplication.java
+++ assignment/src/main/java/com/example/assignment/AssignmentApplication.java
@@ -1,13 +1,8 @@
 package com.example.assignment;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class AssignmentApplication {
 
     public static void main(String[] args) {
         AppLoadConfigProp.showValueConfig("conf/config.properties");
     }
-
 }
